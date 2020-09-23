b04f33a2ecc4367c50935502e577eb363d77f2fa
Duncan Stark <letmunplaysgames@gmail.com>
2020-09-23 19:38:49 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,37 +1,47 @@
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
-  
+ $array = [1,2,3,4]
 end
 
-def add_element_to_end_of_array(array, element)
-  
+def add_element_to_end_of_array(array , element)
+array = ["wow", "I", "am", "really", "learning"]
+    element = "arrays!"
+  array << "arrays!"
 end
 
-def add_element_to_start_of_array(array, element)
-  
+def add_element_to_start_of_array(array, element = "0")
+  array = ["I", "am", "really", "learning"]
+    element = "wow"
+    array.unshift("wow")
 end
 
 def remove_element_from_end_of_array(array)
-  
+  array = ["I", "am", "really", "learning", "arrays!"]
+  array.pop
 end
 
 def remove_element_from_start_of_array(array)
-  
+  array = ["I", "am", "really", "learning", "arrays!"]
+  array.shift(1)
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+  array = ["I", "am", "really", "learning", "arrays!"]
+  index_number = [-2]
+  puts array[-2]
 end
 
 def retrieve_first_element_from_array(array)
-  
+  array = ["I", "am", "really", "learning", "arrays!"]
+  puts array[0]
 end
 
 def retrieve_last_element_from_array(array)
-  
+  array = ["I", "am", "really", "learning", "arrays!"]
+  puts array[-1]
 end
 
 def update_element_from_index(array, index_number, element)
